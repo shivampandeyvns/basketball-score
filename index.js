3c96adf8c34fc5cefbc139ded83059640a04dc4e
Shivam Pandey <77262548+shivampandeyvns@users.noreply.github.com>
2022-08-14 19:21:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function resetScore()
     scoreGuest=0
     homeScore.innerText=scoreHome;
     guestScore.innerText=scoreGuest;
-    leader.innerText="Start The Game";
+    leader.innerText="";
 }
 
 function leaderScore()
